ae35439e7923be1f0823802d8340b06a7426c709
Beau Smith <beau@beausmith.com>
2021-05-24 12:11:43 -0700

    Updates

---
+++ precache-manifest.54d8aa8975cb8cf7c4fff386c77e5fa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29656a34a28e43512c2ee484be8697a",
+    "url": "/election-results-reporting/index.html"
+  },
+  {
+    "revision": "88c30962f55c13e3624c",
+    "url": "/election-results-reporting/static/css/main.4593755f.chunk.css"
+  },
+  {
+    "revision": "3b43af602f185586aa3b",
+    "url": "/election-results-reporting/static/js/2.1ded4ca5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/election-results-reporting/static/js/2.1ded4ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c30962f55c13e3624c",
+    "url": "/election-results-reporting/static/js/main.f6826d57.chunk.js"
+  },
+  {
+    "revision": "154095c7a9a34e339211",
+    "url": "/election-results-reporting/static/js/runtime-main.78b496c7.js"
+  }
+]);
